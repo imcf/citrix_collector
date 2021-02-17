5302c5f98c2ed9c522ec847247f7e133b879f29b
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-02-17 16:27:32 +0100

    Show a startup message

--- citrix_collector.ps1
+++ citrix_collector.ps1
@@ -2,6 +2,8 @@
 $Config = Import-PowerShellDataFile "$PSScriptRoot\Configuration\Config.psd1"
 $FileName = "$($Config.PrometheusCollectorDir)\$($Config.PrometheusCollectorFile)"
 
+Write-Output "Starting the Citrix Prometheus collector..."
+
 while ($True) {
     & $PSScriptRoot\New-CitrixMetrics.ps1 | Out-File $FileName -Encoding "UTF8"
     Start-Sleep -Seconds 30
