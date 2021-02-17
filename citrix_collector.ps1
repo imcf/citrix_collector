01fbf343f797b074db92dd0ec5c4e369effd43bb
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-02-17 16:27:25 +0100

    Make script location-independent

--- citrix_collector.ps1
+++ citrix_collector.ps1
@@ -3,6 +3,6 @@ $Config = Import-PowerShellDataFile "$PSScriptRoot\Configuration\Config.psd1"
 $FileName = "$($Config.PrometheusCollectorDir)\$($Config.PrometheusCollectorFile)"
 
 while ($True) {
-    & .\New-CitrixMetrics.ps1 | Out-File $FileName -Encoding "UTF8"
+    & $PSScriptRoot\New-CitrixMetrics.ps1 | Out-File $FileName -Encoding "UTF8"
     Start-Sleep -Seconds 30
 }
